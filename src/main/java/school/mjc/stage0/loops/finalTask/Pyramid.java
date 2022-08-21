d9488818c7006bd1d30878e951dd58d1a181494b
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-21 12:39:01 +0500

    Update Pyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,43 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+       
+ 
+        int rowCount = 1;
+ 
+        System.out.println("Here Is Your Pyramid");
+ 
+        //Implementing the logic
+ 
+        for (int i = cathetusLength; i > 0; i--)
+        {
+            //Printing i*2 spaces at the beginning of each row
+ 
+            for (int j = 1; j <= i*2; j++)
+            {
+                System.out.print(" ");
+            }
+ 
+            //Printing j value where j value will be from 1 to rowCount
+ 
+            for (int j = 1; j <= rowCount; j++)          
+            {
+                System.out.print(j+" ");
+            }
+ 
+            //Printing j value where j value will be from rowCount-1 to 1
+             
+            for (int j = rowCount-1; j >= 1; j--)
+            {               
+                System.out.print(j+" ");            
+            }                       
+             
+            System.out.println();
+ 
+            //Incrementing the rowCount
+ 
+            rowCount++;
+        }
 
     }
 
