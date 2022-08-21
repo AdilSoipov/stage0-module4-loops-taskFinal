28e116d09d929e887969ea0d51eab8e6294a9434
AdilSoipov <111345038+AdilSoipov@users.noreply.github.com>
2022-08-21 12:41:38 +0500

    Update Power.java

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+     
+
+    long result = 1;
+
+    while (power != 0) {
+      result *= numberToPrint;
+      --power;
+    }
+
+    System.out.println(result);
 
     }
 
